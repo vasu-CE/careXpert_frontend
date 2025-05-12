74dc8c01963fd545db0ad378ec5fc93daecac94a
vasu kamani <vasukamani.ce@gmail.com>
2025-05-12 10:24:40 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+     <div className="bg-red-500 text-white" >hyy</div>
+     <Button onClick={() => alert("Hy")}>hello</Button>
+    </>
+  )
+}
+
+export default App
