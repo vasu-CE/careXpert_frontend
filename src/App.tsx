21cddc6acb872ce6a2802ca5ce64a83b6f11c180
vinitsaspara <sasparavinit@gmail.com>
2025-05-29 01:17:32 +0530

    add login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Button } from "./components/ui/button"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/ui/shared/Navbar";
+import Login from "./components/ui/auth/Login";
+import Signup from "./components/ui/auth/Signup";
+import HomePage from "./components/ui/pages/HomePage";
+
 
 function App() {
-
   return (
-    <>
-     <div className="bg-red-500 text-white" >hyy</div>
-     <Button onClick={() => alert("Hy")}>hello</Button>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
