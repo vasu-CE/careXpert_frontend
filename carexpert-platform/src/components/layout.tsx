da555fad2b3790aea42f597769cf812174c2d4f9
vasu kamani <vasukamani.ce@gmail.com>
2025-06-16 13:37:45 +0530

    delete

--- carexpert-platform/src/components/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function Layout({ children }: { children?: React.ReactNode }) {
-  return <div className="min-h-screen font-sans">{children || <Outlet />}</div>;
-}
